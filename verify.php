6e39393a59359b3a080ea7a4e290570e04cb62c1
supeenuns <safety2824@hotmail.com>
2017-03-31 16:57:13 +0700

    update file
    update proxy

--- verify.php
+++ verify.php
@@ -1,4 +1,7 @@
 <?php
+$proxy = 'proxyurl:port';
+$proxyauth = 'username:password';
+
 $access_token = 'tHAk5YYeatup9nmdPCPpif4VKpR5yK61EMihBOwbspU2iIUMDEnMLLXrNEYUP+Witqr1fxeyX80ot4pjGjw+xgTrJuVA+Fn9LD+7oO/uDaJIvC0t8D30WTfgI/m7XcW2x4nJkAVPYjxhFuGnNKTKRwdB04t89/1O/w1cDnyilFU=';
 
 $url = 'https://api.line.me/v1/oauth/verify';
@@ -9,6 +12,11 @@ $ch = curl_init($url);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+
+curl_setopt($ch, CURLOPT_PROXY, $proxy);
+curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+
 $result = curl_exec($ch);
 curl_close($ch);
 
