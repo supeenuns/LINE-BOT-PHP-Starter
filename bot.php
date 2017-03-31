5535d6281924a7a54ed3f7465a773e7b2bc9407b
supeenuns <safety2824@hotmail.com>
2017-03-31 17:08:07 +0700

    update proxy bot file
    update proxy bot file

--- bot.php
+++ bot.php
@@ -1,4 +1,7 @@
 <?php
+$proxy = 'proxyurl:http://fixie:omEUpEOuDHU56NW@velodrome.usefixie.com:80';
+$proxyauth = 'supeenuns:sup280228';
+
 $access_token = 'tHAk5YYeatup9nmdPCPpif4VKpR5yK61EMihBOwbspU2iIUMDEnMLLXrNEYUP+Witqr1fxeyX80ot4pjGjw+xgTrJuVA+Fn9LD+7oO/uDaJIvC0t8D30WTfgI/m7XcW2x4nJkAVPYjxhFuGnNKTKRwdB04t89/1O/w1cDnyilFU=';
 
 // Get POST body content
@@ -37,6 +40,10 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+                        curl_setopt($ch, CURLOPT_PROXY, $proxy);
+                        curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+
 			$result = curl_exec($ch);
 			curl_close($ch);
 
